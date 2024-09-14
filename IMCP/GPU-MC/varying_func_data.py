8dfa40edad3dc05de6b3ba8448c90fd82c52c5d2
the-wiggler <tyler4077@icloud.com>
2024-09-13 21:40:39 -0400

    delete bloat

--- IMCP/GPU-MC/varying_func_data.py
+++ IMCP/GPU-MC/varying_func_data.py
@@ -8,3 +8,6 @@ std_list1 = np.array(np.load(os.path.join('np_store_GPU', 'std_list_GPU.npy')))
 history_count_list1 = np.array(np.load(os.path.join('np_store_GPU', 'history_count_list_GPU.npy')))
 batch_times1 = np.array(np.load(os.path.join('np_store_GPU', 'batch_times_GPU.npy')))
 
+np.savetxt("calc_int.csv", avg_calc_int1, delimiter = ",")
+np.savetxt("stdv_list.csv", std_list1, delimiter = ",")
+np.savetxt("batch_times.csv", batch_times1, delimiter = ",")
