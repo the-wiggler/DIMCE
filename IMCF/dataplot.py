8616aae700841dd2d0a2c83746a4b2a5d635b61c
t-wiggle <tyler4077@icloud.com>
2024-09-14 22:42:02 -0400

    yet

--- IMCF/dataplot.py
+++ IMCF/dataplot.py
@@ -2,31 +2,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("results.csv")
-data2 = pd.read_csv("results2.csv")
 # batch
 # history
 # calc_int
 # stddev
 # batch_time
 
-# # Plot stddev
-# plt.figure()
-# plt.scatter(data['history'], data['stddev'], s=1, color='r')
-# plt.title("Stddev vs History")
-# plt.xlabel("History")
-# plt.ylabel("Stddev")
+# Plot stddev
+plt.figure()
+plt.scatter(data['history'], data['stddev'], s=1, color='r')
+plt.title("Stddev vs History")
+plt.xlabel("History")
+plt.ylabel("Stddev")
 
-# # Plot calc_int
-# plt.figure()
-# plt.scatter(data['history'], data['calc_int'], s=1, color='r')
-# plt.title("Calc Int vs History")
-# plt.xlabel("History")
-# plt.ylabel("Calc Int")
+# Plot calc_int
+plt.figure()
+plt.scatter(data['history'], data['calc_int'], s=1, color='r')
+plt.title("Calc Int vs History")
+plt.xlabel("History")
+plt.ylabel("Calc Int")
 
-# Plot batch_time
+# # Plot batch_time
 plt.figure()
 plt.scatter(data['history'], data['batch_time'], s=1, color='r')
-plt.scatter(data2['history2'], data2['batch_time2'], s=1, color='b')
 plt.title("Batch Time vs History")
 plt.xlabel("History")
 plt.ylabel("Batch Time")
