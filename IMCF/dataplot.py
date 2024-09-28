ef5dec702be151d5ece1db30627178e6d106db67
t-wiggle <tyler4077@icloud.com>
2024-09-28 01:25:03 -0400

    added time feature and reorganized how plots are ararnged in python

--- IMCF/dataplot.py
+++ IMCF/dataplot.py
@@ -3,68 +3,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 data = pd.read_csv("results.csv")
-# data2 = pd.read_csv("resultsSERIAL.csv")
 
+fig, axes = plt.subplots(2, 3, figsize=(15, 10))
 
-# Plot stddev
-plt.figure(1)
-plt.scatter(data['history_count'], data['variance'], s=0.4, color='r')
-plt.title(r"Integration of $\int_{0}^{1} \frac{\ln(1+x)}{x}\, dx$ ~ History Count v. Variance of Integration", fontsize=14)
-plt.xlabel("History Count")
-plt.ylabel("Variance")
-plt.xscale('log')
-plt.yscale('log')
+# Plot variance
+axes[0, 0].scatter(data['history_count'], data['variance'], s=0.4, color='b')
+axes[0, 0].set_title("History Count v. Variance", fontsize=10)
+axes[0, 0].set_xlabel("History Count")
+axes[0, 0].set_ylabel("Variance")
+axes[0, 0].set_xscale('log')
+axes[0, 0].set_yscale('log')
 
-plt.figure(3)
-plt.scatter(data['history_count'], data['stdv'], s=0.4, color='r')
-plt.title(r"Integration of $\int_{0}^{1} \frac{\ln(1+x)}{x}\, dx$ ~ History Count v. Standard Deviation of Integration", fontsize=14)
-plt.xlabel("History Count")
-plt.ylabel("Standard Deviation")
-plt.xscale('log')
-plt.yscale('log')
+# Plot standard deviation
+axes[0, 1].scatter(data['history_count'], data['stdv'], s=0.4, color='r')
+axes[0, 1].set_title("History Count v. Standard Deviation", fontsize=10)
+axes[0, 1].set_xlabel("History Count")
+axes[0, 1].set_ylabel("Standard Deviation")
+axes[0, 1].set_xscale('log')
+axes[0, 1].set_yscale('log')
 
-# plt.figure(2)
-# plt.scatter(data['history_count'], data['rel_err'], s=1, color='r')
-# plt.title(r"Integration of $\int_{0}^{1} \frac{\ln(1+x)}{x}\, dx$ ~ History Count v. Relative Error", fontsize=14)
-# plt.xlabel("History Count")
-# plt.ylabel("Relative Error")
+# Plot relative error
+axes[0, 2].scatter(data['history_count'], data['rel_err'], s=1, color='r')
+axes[0, 2].set_title("History Count v. Relative Error", fontsize=10)
+axes[0, 2].set_xlabel("History Count")
+axes[0, 2].set_ylabel("Relative Error")
 
+# Plot batch time
+axes[1, 0].scatter(data['history_count'], data['batch_time'], s=1, color='g')
+axes[1, 0].set_title("History Count v. Batch Time", fontsize=10)
+axes[1, 0].set_xlabel("History Count")
+axes[1, 0].set_ylabel("Batch Time")
+axes[1, 0].set_xscale('log')
+axes[1, 0].set_yscale('log')
 
+# Plot batch time vs relative error
+axes[1, 1].scatter(data['batch_time'], data['rel_err'], s=1, color='g')
+axes[1, 1].set_title("Batch Time v. Relative Error", fontsize=10)
+axes[1, 1].set_xlabel("Batch Time")
+axes[1, 1].set_ylabel("Relative Error")
+axes[1, 1].set_xscale('log')
+axes[1, 1].set_yscale('log')
 
-# # Plot calc_int
-# plt.figure(2)
-# plt.scatter(data['history'], data['calc_int'], s=1, color='r')
-# plt.title(r"Integration of $\int_{0}^{1} \frac{\ln(1+x)}{x}\, dx$ ~ History Count v. Calculated Integration", fontsize=14)
-# plt.xlabel("History Count")
-# plt.ylabel("Calculated Integration")
+fig.delaxes(axes[1, 2])
 
-# # # Plot batch_time
-# plt.figure(3)
-# plt.scatter(data['history'], data['batch_time'], s=1, color='r')
-# plt.scatter(data2['history'], data2['batch_time'], s=1, color='b')
-# plt.title(r"Integration of $\int_{0}^{1} \frac{\ln(1+x)}{x}\, dx$ ~ History Count v. Batch Time", fontsize=14)
-# plt.gca().set_title('*Using OpenMP Parallelization', loc='right', pad=20)
-# plt.xlabel("History Count")
-# plt.ylabel("Batch Time")
-
-# plt.figure(4)
-# plt.scatter(data['history'], data['percent_err'], s=1, color='r')
-# plt.title(r"Integration of $\int_{0}^{1} \frac{\ln(1+x)}{x}\, dx$ ~ History Count v. Percent Error from Known Value", fontsize=14)
-# plt.gca().set_title('*Using OpenMP Parallelization', loc='right', pad=20)
-# plt.xlabel("History Count")
-# plt.ylabel("Percent Error")
-
-# data = pd.read_csv("varbatchOMP.csv")
-# data2 = pd.read_csv("varbatchSERIAL.csv")
-
-# plt.figure()
-# plt.plot(data['batches'], data[' compute_time'], color='r', marker='o', label='OMP Parallelization Method')
-# plt.plot(data2['batches'], data2[' compute_time'], color='b', marker='o', label='Serial Computation')
-# plt.title(r"Integration Time of $\int_{0}^{1} \frac{\ln(1+x)}{x}\, dx$ With Varying Batches", fontsize=14)
-# plt.gca().set_title('Histories Per Batch: 10000000', loc='right', pad=20)
-# plt.xlabel("Batches")
-# plt.ylabel("Compute Time")
-# plt.legend()  # Add a legend to differentiate lines
-# plt.grid(True)  
+plt.tight_layout()
 
 plt.show()
--- IMCF/integralMCF.f90
+++ IMCF/integralMCF.f90
@@ -2,31 +2,32 @@ program integralMCF
     use omp_lib
     implicit none
     integer, parameter :: dp = selected_real_kind(15, 307)
-    integer(dp) :: i, j, k, batches, histories, hist_sum
-    real(dp) :: x, a, b, IMC, mean, hist_real, lit_val
-    real(dp), allocatable :: IMC_val(:), r_val(:), variance(:), stdv(:), rel_err(:)
+    integer(dp) :: i, j, k, batches, histories, sum_hist
+    real(dp) :: x, a, b, IMC, mean, hist_real, lit_val, start_time, end_time
+    real(dp), allocatable :: IMC_val(:), r_val(:), variance(:), stdv(:), rel_err(:), batch_time(:)
     integer(dp), allocatable :: history_count(:)
     call random_seed()
 
     a = 0.0_dp ! lower range of integration
     b = 5.0_dp ! upper range of integration
-    batches = 800 ! how many integrations to compute
+    batches = 10000 ! how many integrations to compute
     histories = 1000
     lit_val = 0.52791728116532241384461568_dp
-
-    allocate(IMC_val(batches), history_count(batches), variance(batches), stdv(batches), rel_err(batches))
+! HEY YOU SHOULD ADD A CONFIDENCE INTERVAL FEATURE!!! *&()*@()#*()@&)!(*#$&#*)(!@#$*&()#@*&)($*&()^*)B^&(#)B&*(^@#)*($^*@#)$^@)#@#$*)(@#(*$*@#()$))
+    allocate(IMC_val(batches), history_count(batches), variance(batches), stdv(batches), rel_err(batches), batch_time(batches))
 
     ! Creates an array of histories that each batch takes its respective index of
     do k = 1, batches
         history_count(k) = histories
-        histories = histories * 1.01
+        histories = histories * 1.001
     end do
 
-    hist_sum = sum(history_count)
+    sum_hist = sum(history_count)
 
-    !$OMP PARALLEL DO PRIVATE(j, i, histories, x, r_val, IMC) &
-    !$OMP& SHARED(a, b, IMC_val, batches)
+    !$OMP PARALLEL DO PRIVATE(j, i, histories, x, r_val, IMC, start_time, end_time) &
+    !$OMP& SHARED(a, b, IMC_val, batches, batch_time, lit_val)
     do j = 1, batches
+        start_time = omp_get_wtime()
         ! BEGIN CALCULATION ***********************************************************************************
         histories = history_count(j)
         allocate(r_val(histories))
@@ -38,10 +39,14 @@ program integralMCF
 
         IMC = (b - a) * (sum(r_val) / histories) ! CALCULATES INTEGRAL
         ! END CALCULATION *************************************************************************************
+        end_time = omp_get_wtime()
+        batch_time(j) = end_time - start_time
+
         IMC_val(j) = IMC
         deallocate(r_val)
-        print *, 'Batch:', j, ' | Histories:', history_count(j), ' | Remaining Histories:', hist_sum
-        hist_sum = hist_sum - histories
+
+        sum_hist = sum_hist - histories
+        print *, 'Remaining Histories:',sum_hist
     end do
     !$OMP END PARALLEL DO
 
@@ -50,15 +55,15 @@ program integralMCF
         hist_real = real(j)
         variance(j) = sum((IMC_val(1:j) - mean) ** 2) / hist_real  ! Calculate variance
         stdv(j) = sqrt(variance(j))
-        rel_err(j) = abs((IMC - lit_val)) / lit_val
+        rel_err(j) = abs((IMC_val(j) - lit_val)) / lit_val
     end do
 
     ! Write arrays to CSV
     open(unit=1, file='results.csv', status='replace')
-    write(1,*) 'batch,IMC_val,history_count,variance,stdv,rel_err'
+    write(1,*)'batch,IMC_val,history_count,variance,stdv,rel_err,batch_time'
     do j = 1, batches
-        write(1, '(I0,",",ES15.7,",",I0,",",ES15.7,",",ES15.7,",",ES15.7)') &
-              j, IMC_val(j), history_count(j), variance(j), stdv(j), rel_err(j)
+        write(1, '(I0,",",ES15.7,",",I0,",",ES15.7,",",ES15.7,",",ES15.7,",",ES15.7)') &
+              j, IMC_val(j), history_count(j), variance(j), stdv(j), rel_err(j), batch_time(j)
     end do
     close(1)
     print *, 'CSV file written successfully!'
